1af9a1a286f09679000d9bc3ae9d74ea726c49fe
nicoescudero <nicolas.escudero00@gmail.com>
2023-05-05 10:35:05 -0300

    fix: menu

--- Ejercicio7/menu.py
+++ Ejercicio7/menu.py
@@ -3,7 +3,7 @@ from colorama import Fore, Back, Style
 
 def main_menu():
     print('*****************************************')
-    print('*' + Fore.YELLOW+Back.LIGHTCYAN_EX+Style.BRIGHT + '\tEjercicio 5' + Back.RESET+Style.RESET_ALL + '\n*')
+    print('*' + Fore.YELLOW+Back.LIGHTCYAN_EX+Style.BRIGHT + '\tEjercicio 7' + Back.RESET+Style.RESET_ALL + '\n*')
     print('*\t 1)Comparar cantidad de millas / Buscar viajero que tenga determinada cantidad de millas')
     print('*\t 2)Acumular millas')
     print('*\t 3)Canjear millas')
